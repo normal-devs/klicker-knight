ee7e7d7a512fc6c7e739ecfa13353eccf2710485
Rachid Lamouri <rachidlamouri@gmail.com>
2021-11-30 01:21:44 -0500

    Port scaffolded oclif tests
    - migrated mocha.opts to mocharc.json
    - added index.ts wrapper to klickerKnight.ts to allow klickerKnight.ts
    to be imported with 'import' instead of 'require'

---
+++ .mocharc.js
@@ -0,0 +1,5 @@
+module.exports = {
+  require: 'ts-node/register',
+  recursive: true,
+  timeout: 5000,
+}
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,3 @@
-import {Command} from '@oclif/command'
+import {KlickerKnight} from './klickerKnight';
 
-class KlickerKnight extends Command {
-  async run() {
-    this.log('You helplessly stare into the void as you try to gain your bearings. Your foot catches on what you believe to be a loose flagstone. You fall and break your neck. You are dead.');
-  }
-}
-
-export = KlickerKnight
+export = KlickerKnight;
