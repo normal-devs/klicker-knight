f713731b881146078e811022581500dfa101b8c7
Rachid Lamouri <rachidlamouri@gmail.com>
2021-12-02 00:41:15 -0500

    Configure eslint with airbnb and typescript rules

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  "extends": [
+    'airbnb-base',
+    'airbnb-typescript/base'
+  ],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "project": ["./tsconfig.linters.json", "tsconfig.json", "tests/tsconfig.json"],
+  },
+  "plugins": ["@typescript-eslint"],
+  "rules": {
+    "max-len": [
+      "error",
+      120,
+      2,
+      {
+        "ignoreUrls": true,
+        "ignoreComments": false,
+        "ignoreRegExpLiterals": true,
+        "ignoreStrings": true,
+        "ignoreTemplateLiterals": true,
+        "ignoreTrailingComments": true,
+      }
+    ],
+  },
+}
