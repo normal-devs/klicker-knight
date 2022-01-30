c264e41f995497fc3845591f1452d1be96ad0cdd
Rachid Lamouri <rachidlamouri@gmail.com>
2022-01-30 11:57:30 -0500

    Fix semantic-mocha test config linting error

--- semantic-mocha/tests/.eslintrc.js
+++ semantic-mocha/tests/.eslintrc.js
@@ -5,6 +5,7 @@ module.exports = {
   extends: ['../../.eslintrc.js'],
   rules: {
     'import/no-default-export': 'off',
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
     'mocha/no-setup-in-describe': 'off',
   },
 };
