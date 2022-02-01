8622199598d5ed0640b3a2ba2846e4cb9a545a0d
Rachid Lamouri <rachidlamouri@gmail.com>
2022-02-01 00:18:42 -0500

    Enforce sort-keys in tests/.eslintrc.js

--- tests/.eslintrc.js
+++ tests/.eslintrc.js
@@ -1,9 +1,11 @@
+/* eslint sort-keys: 'error' */
+
 // eslint-disable-next-line import/no-commonjs
 module.exports = {
   extends: ['../.eslintrc.js'],
   rules: {
-    'mocha/max-top-level-suites': 0,
     'func-names': 0,
+    'mocha/max-top-level-suites': 0,
     'mocha/no-hooks-for-single-case': 0,
   },
 };
