e9e03aa3952ddb5c7bdcc801db74ce0fe71e36a9
Rachid Lamouri <rachidlamouri@gmail.com>
2022-02-01 21:47:20 -0500

    Move eslint ignored files to appropriate config

--- eslint-local-rules/.eslintrc.js
+++ eslint-local-rules/.eslintrc.js
@@ -3,6 +3,7 @@
 // eslint-disable-next-line import/no-commonjs
 module.exports = {
   extends: ['../.eslintrc.js'],
+  ignorePatterns: ['index.js', 'dist/*'],
   rules: {
     'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
   },
