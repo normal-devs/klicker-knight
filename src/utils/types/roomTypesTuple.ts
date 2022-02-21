57645c9f970e36b53491a9473fa047f5d59b01fd
Rachid Lamouri <rachidlamouri@gmail.com>
2022-02-21 13:32:52 -0500

    Add roomception room schema and update types

--- src/utils/types/roomTypesTuple.ts
+++ src/utils/types/roomTypesTuple.ts
@@ -2,12 +2,18 @@
 // Use "npm run compile:gameStateSchema" to rebuild
 
 // eslint-disable-next-line import/no-restricted-paths
-import { ExampleRoom1, ExampleRoom2, ExampleRoom3 } from './gameState';
+import {
+  ExampleRoom1,
+  ExampleRoom2,
+  ExampleRoom3,
+  RoomceptionRoom,
+} from './gameState';
 
 export const ROOM_TYPES_TUPLE = [
   'exampleRoom1',
   'exampleRoom2',
   'exampleRoom3',
+  'roomceptionRoom',
 ] as const;
 
 export type RoomTypesTuple = typeof ROOM_TYPES_TUPLE;
@@ -21,4 +27,6 @@ export type NarrowedRoomState<TRoomType extends RoomType> =
     ? ExampleRoom2
     : TRoomType extends 'exampleRoom3'
     ? ExampleRoom3
+    : TRoomType extends 'roomceptionRoom'
+    ? RoomceptionRoom
     : never;
